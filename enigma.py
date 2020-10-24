957f9ecf464175651e6dd698bfe60ceede221521
Justin <37479575+Equinox4@users.noreply.github.com>
2020-10-25 00:02:19 +0200

    amélioration de la structure qui représente les rotors

--- enigma.py
+++ enigma.py
@@ -2,92 +2,70 @@
 
 from string import ascii_uppercase
 from collections import deque
-from random import seed
-from random import sample
+from random import seed, sample
 from sys import exit
 
 
 ALPHABET = list(ascii_uppercase)
-SEED = 5 # change seed equals to change rotors
+ROTORS_CONF = [
+    { 'seed': '7', 'position': 'U' },
+    { 'seed': '4', 'position': 'H' },
+    { 'seed': '9', 'position': 'G' },
+]
+PERMUTATIONS = {
+    'J': 'K',
+    'V': 'C'
+} # two pairs of letters that will be permuted
 ROTORS = list()
 
-ROTORS_POSITIONS = 'UHG' # initial rotors position
-PERMUTATIONS = 'JKVC' # two pairs of letter that will be permuted
+def get_initialized_rotors():
+    rotors = list()
+    for num_rotor in range(len(ROTORS_CONF)):
+        rotor = make_rotor(ROTORS_CONF[num_rotor]['seed'])
+        rotor = set_rotor_position(rotor, ROTORS_CONF[num_rotor]['position'])
+        rotors.append(rotor)
 
-#temp
-nb_rotations_rotor1 = 0
-nb_rotations_rotor2 = 0
+    return rotors
 
+def make_rotor(my_seed):
+    seed(my_seed)
 
+    return sample(ALPHABET, len(ALPHABET))
 
-def initRotors(nb_rotors = 3):
+def set_rotor_position(rotor, rotor_position):
+    rotor = deque(rotor)
+    while rotor[0] != rotor_position:
+        rotor.rotate(1)
 
-	for num_rotor in range(nb_rotors):
-
-		rotor = shuffle(num_rotor + SEED)
-		rotor = setRotorPosition(rotor, ROTORS_POSITIONS[num_rotor])
-		ROTORS.append(rotor)
-
-
-def setRotorPosition(rotor, rotor_position):
-
-	rotor = deque(rotor)
-	while rotor[0] != rotor_position:
-		rotor.rotate(1)
-
-	return list(rotor)
-
-def shuffle(my_seed):
-
-	seed(my_seed)
-	return sample(ALPHABET, len(ALPHABET))
-
+    return list(rotor)
 
 def encrypt(text):
+    input_list = list(text)
+    output_list = list()
+    if not verify(text_list):
+        print('Your input text contain a letter that is not supported.') # afficher l'alphabet
+        exit(1)
+    for letter in text:
+        output_list.append(process(letter))
 
-	text_list = list(text)
-	if not verify(text_list):
-		exit(1)
-	for letter in text:
-		
+    return str(output_list)
 
-		
-#############################
-#temporary functions
+def decrypt():
+    pass
+
+def process(letter):
+        index_rotor1 = ROTORS[0].index(letter)
+        permut = ROTORS[1]
+
 def verify(text):
+    for letter in text:
+        if letter not in ALPHABET:
+            return False
 
-	for letter in text:
-	
-		if letter not in ALPHABET:
-			return False
-		
-	return True
+    return True
 
-def setPermutation(letter_couple):
+def rotate_rotors():
+    pass
 
-	pass
-	
-def rotate(rotor1, rotor2, rotor3):
 
-	pass	
-
-def substitute(letter, rotor):
-
-	pass
-	
-def process(msg_in):
-
-	msg_out = list()
-	for letter in list(msg_in):
-		index_rotor1 = ROTORS[0].index(letter)
-		permut = ROTORS[1]
-		
-	return msg_out
-
-##########
-
-initRotors()
-text_in = input("Hello World !")
-text_out = encrypt(text_in)
-print(text_out)
-
+ROTORS = get_initialized_rotors()
